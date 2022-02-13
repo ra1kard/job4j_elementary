055908f12914e06cd7b7e51245b89bd5e73e9a84
Stanislav Kufarev <91raykard@gmail.com>
2022-02-13 17:04:53 +0300

    Fix - нарушения по стилю checkstyle.

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    public static void plus (int first, int second) {
+    public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
