02a798cadfcb68365a7cf488026c56030c5d7c26
ra1kard <91raykard@gmail.com>
2022-02-15 02:09:11 +0300

    test commit from work pc 6

--- src/main/java/ru/job4j/StoronyRa.java
+++ src/main/java/ru/job4j/StoronyRa.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class StoronyRa {
 
     public static void main(String[] args) {
-        System.out.println("Лочи стелит");
+        System.out.println("Лочи стелит!!!");
     }
 
 }
