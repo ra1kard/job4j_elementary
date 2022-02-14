4d15459da216fd70f45a88654019642773ab45ac
ra1kard <91raykard@gmail.com>
2022-02-14 21:09:47 +0300

    test commit from work pc

--- src/main/java/ru/job4j/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("new line");
-        System.out.println("new line");
-    }
-}
