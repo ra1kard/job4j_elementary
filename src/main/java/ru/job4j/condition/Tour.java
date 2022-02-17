f62397ac72b2ca9185ce7003051a8fc604b40d93
Stanislav Kufarev <91raykard@gmail.com>
2022-02-17 20:22:29 +0300

    4.1.0. Оператор if с блоком else [#227534]

---
+++ src/main/java/ru/job4j/condition/Tour.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Tour {
+
+    public static void offer(boolean passport) {
+        System.out.println("A client has foreign passport : " + passport);
+        if (passport) {
+            System.out.println("Tours abroad.");
+        } else {
+            System.out.println("Tours in Russia.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Tour.offer(true);
+        Tour.offer(false);
+    }
+
+}
