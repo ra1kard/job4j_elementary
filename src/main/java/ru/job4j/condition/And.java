16f9a313526b12821c9f34c1555411b0370f378e
Stanislav Kufarev <91raykard@gmail.com>
2022-02-18 14:11:23 +0300

    4.3.1. Оператор && [#333783]

---
+++ src/main/java/ru/job4j/condition/And.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class And {
+
+    public static void main(String[] args) {
+        boolean first = true;
+        boolean second = false;
+        boolean join = first && second;
+        System.out.println(join);
+    }
+
+}
