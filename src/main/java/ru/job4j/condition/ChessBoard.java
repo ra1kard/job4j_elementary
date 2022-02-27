9c4c41db15d23acdd48616ff673eea90032965d1
Stanislav Kufarev <91raykard@gmail.com>
2022-02-27 19:56:58 +0300

    4.3.6. Math.abs [#373616]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
 public class ChessBoard {
-
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
         if (isValid(x1) && isValid(x2) && isValid(y1) && isValid(y2)) {
@@ -14,6 +13,10 @@ public class ChessBoard {
 
     private static boolean isValid(int coordinate) {
         return coordinate >= 0 && coordinate <= 7;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(way(6, 7, 1, 2));
     }
 
 }
