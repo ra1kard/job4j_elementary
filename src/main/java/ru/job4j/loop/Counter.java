5fce27dddf7e6c70e7259729274fbd0d2c501917
Stanislav Kufarev <91raykard@gmail.com>
2022-03-09 07:31:27 +0300

    5.0.1. Сумма чисел. [#226416]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -5,7 +5,7 @@ public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         int i;
-        for (i = start; i < finish; i++) {
+        for (i = start; i <= finish; i++) {
             sum = sum + i;
         }
         return sum;
