4ab28170410c12efd3f289c4a0bbeb0256443c6d
Stanislav Kufarev <91raykard@gmail.com>
2022-03-09 07:43:50 +0300

    5.0.2. Вечный цикл for. [#226417 #273157]

---
+++ src/main/java/ru/job4j/loop/InfinityLoop.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class InfinityLoop {
+
+    public static void main(String[] args) {
+        for (int index = 0; index < 10; index++) {
+            System.out.println("infinity");
+            index = index - 1;
+        }
+    }
+
+}
