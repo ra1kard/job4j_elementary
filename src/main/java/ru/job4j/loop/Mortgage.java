38d09c1c945e5b8d0f67c281be311aa337b812dc
Stanislav Kufarev <91raykard@gmail.com>
2022-03-19 12:34:08 +0300

    5.7. Ипотека [#156315 #276971]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
 
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        while (amount + amount * (percent / 100) > 0) {
+        while (amount > 0) {
             amount = (amount + amount * (percent / 100)) - salary;
             year++;
         }
