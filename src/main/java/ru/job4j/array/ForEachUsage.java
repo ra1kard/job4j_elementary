b5a7567d571ddfe5dfb7dbf453b6986aa3278f3a
Stanislav Kufarev <91raykard@gmail.com>
2022-03-20 19:15:09 +0300

    6.0.3.1. Массивы и цикл for-each [#333581]

---
+++ src/main/java/ru/job4j/array/ForEachUsage.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ForEachUsage {
+
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three", "four", "five"};
+        for (String number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+}
