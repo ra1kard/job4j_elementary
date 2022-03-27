e50d2a956265e760f7fed71bacd7979f7737015d
Stanislav Kufarev <91raykard@gmail.com>
2022-03-27 23:06:35 +0300

    Двумерный массив пример

---
+++ src/main/java/ru/job4j/array/TwoArray.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+public class TwoArray {
+
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
