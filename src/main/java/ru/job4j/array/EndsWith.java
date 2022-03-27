33b22296b6462a05f1d0d547db38a8732d71b674
Stanislav Kufarev <91raykard@gmail.com>
2022-03-27 19:50:34 +0300

    6.4.2. Слово заканчивается на ... [#156317 #278944]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -5,9 +5,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            if (word[word.length - 1 - index] == post[post.length - 1 - index]) {
-                result = true;
-            } else {
+            if (word[word.length - 1 - index] != post[post.length - 1 - index]) {
                 result = false;
                 break;
             }
