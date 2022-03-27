17b2513f9491a17065aa88d6f4ed92fa2bea763c
Stanislav Kufarev <91raykard@gmail.com>
2022-03-27 19:46:01 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619 #278919]

--- src/main/java/ru/job4j/array/PrintEvenElements.java
+++ src/main/java/ru/job4j/array/PrintEvenElements.java
@@ -2,21 +2,16 @@ package ru.job4j.array;
 
 public class PrintEvenElements {
 
-    public static String printEvenInds(int[] numbers) {
+    public static String printEvenInts(int[] numbers) {
         String ln = System.lineSeparator();
         String res = "Элементы массива с чётными индексами, начиная с последнего: " + ln;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[numbers.length - 1 - i] % 2 == 0) {
+            if ((numbers.length - 1 - i) % 2 == 0) {
                 res += numbers[numbers.length - 1 - i] + ln;
                 System.out.println(numbers[numbers.length - 1 - i]);
             }
         }
         return res;
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-        printEvenInds(numbers);
     }
 
 }
--- src/test/java/ru/job4j/array/PrintEvenElementsTest.java
+++ src/test/java/ru/job4j/array/PrintEvenElementsTest.java
@@ -10,8 +10,8 @@ public class PrintEvenElementsTest {
         String ln = System.lineSeparator();
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
-                + 14 + ln + 12 + ln + 10 + ln + 8 + ln + 6 + ln + 4 + ln + 2 + ln;
-        assertEquals(expected, PrintEvenElements.printEvenInds(numbers));
+                + 15 + ln + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
+        assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
     }
 
     @Test
@@ -19,8 +19,8 @@ public class PrintEvenElementsTest {
         String ln = System.lineSeparator();
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
         String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
-                + 14 + ln + 12 + ln + 10 + ln + 8 + ln + 6 + ln + 4 + ln + 2 + ln;
-        assertEquals(expected, PrintEvenElements.printEvenInds(numbers));
+                + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
+        assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
     }
 
 }
