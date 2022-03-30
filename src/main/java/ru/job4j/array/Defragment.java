967a008f23d1d1f0308e7ac20d8c16cfc7eaef05
Stanislav Kufarev <91raykard@gmail.com>
2022-03-30 21:15:31 +0300

    6.8. Дефрагментация массива [#225]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class Defragment {
+
+    public static String[] compress(String[] array) {
+        int temp = 0;
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = index + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        array[index] = array[i];
+                        array[i] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+
+}
---
+++ src/test/java/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DefragmentTest {
+
+    @Test
+    public void singleFirstNull() {
+        String[] input = {null, "I"};
+        String[] result = Defragment.compress(input);
+        String[] expected = {"I", null};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void notFirstNull() {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] result = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void firstNull() {
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+        String[] result = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void notAllNull() {
+        String[] input = {null, null, null};
+        String[] result = Defragment.compress(input);
+        String[] expected = {null, null, null};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void notAllAtFirstNull() {
+        String[] input = {null, null, null, "I", "wanna", "be", "compressed"};
+        String[] result = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+}
