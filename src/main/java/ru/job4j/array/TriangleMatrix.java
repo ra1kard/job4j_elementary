286c8e79e7c5cff466945216e2f0a3d5827ec5af
Stanislav Kufarev <91raykard@gmail.com>
2022-04-02 03:09:31 +0300

    Job4j Упражнения 1.1.6. Массивы

---
+++ src/main/java/ru/job4j/array/TriangleMatrix.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class TriangleMatrix {
+
+    public static int[][] rows(int count) {
+        int[][] triangle = new int[count][];
+        int cell = 1;
+        int value = 1;
+
+        for (int i = 0; i < triangle.length; i++) {
+            triangle[i] = new int[cell];
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = value;
+                value++;
+            }
+            cell++;
+        }
+
+        for (int i = 0; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                System.out.print(triangle[i][j]);
+            }
+            System.out.println();
+        }
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        TriangleMatrix.rows(3);
+    }
+
+}
