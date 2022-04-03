58d4127f6abe34ec282ede0a695dc1ea6f0228b8
Stanislav Kufarev <91raykard@gmail.com>
2022-04-03 21:21:08 +0300

    Job4j Упражнения 1.1.6. Массивы

---
+++ src/main/java/ru/job4j/array/OddPrint.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class OddPrint {
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        OddPrint.print(new int[] {1, 2, 3});
+    }
+
+}
