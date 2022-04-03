8dcb416eadac29f7d27764aa25904882de85d993
Stanislav Kufarev <91raykard@gmail.com>
2022-04-03 20:20:14 +0300

    Job4j Упражнения 1.1.6. Массивы

---
+++ src/main/java/ru/job4j/array/SummingPrevious.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class SummingPrevious {
+
+    public static int[] calculate(int a, int b, int n) {
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 0) {
+                numbers[i] = a;
+            } else if (i == 1) {
+                numbers[i] = b;
+            } else {
+                for (int j = 0; j < i; j++) {
+                    numbers[i] += numbers[j];
+                }
+            }
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        SummingPrevious.calculate(1, 2, 5);
+    }
+
+}
