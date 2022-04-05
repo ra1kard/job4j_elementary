8e2b95cb901a9af1513b72896ee597620b3016db
Stanislav Kufarev <91raykard@gmail.com>
2022-04-05 19:54:57 +0300

    Job4j Упражнения 1.1.6. Массивы

---
+++ src/main/java/ru/job4j/array/SwapCols.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+public class SwapCols {
+
+    public static void swap(int[][] data, int src, int dst) {
+        int[] temp = new int[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if (j == src) {
+                    temp[j] = data[i][j];
+                    data[i][j] = data[i][dst];
+                    data[i][dst] = temp[j];
+                }
+            }
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] data = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        SwapCols.swap(data, 0, 2);
+    }
+
+}
