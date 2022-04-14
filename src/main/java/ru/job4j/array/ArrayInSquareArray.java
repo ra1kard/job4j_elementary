cfbcbdc1c94d2fe90d2ea9172849062e76db0ca0
Stanislav Kufarev <91raykard@gmail.com>
2022-04-14 16:31:01 +0300

    3.3. Превратить одномерный массив в двумерный.

---
+++ src/main/java/ru/job4j/array/ArrayInSquareArray.java
@@ -0,0 +1,41 @@
+package ru.job4j.array;
+
+public class ArrayInSquareArray {
+
+    public static int[][] convertArray(int[] array) {
+        int result = 0;
+        int n = 0;
+        while (result < array.length) {
+            n++;
+            result = n * n;
+        }
+
+        int[][] arraySecond = new int[n][n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (count < array.length) {
+                    arraySecond[i][j] = array[count];
+                    count++;
+                } else {
+                    arraySecond[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < arraySecond.length; i++) {
+            for (int j = 0; j < arraySecond[i].length; j++) {
+                System.out.print(arraySecond[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        return arraySecond;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};                    // длина = 7
+        int[][] rsl = ArrayInSquareArray.convertArray(array);
+    }
+
+}
