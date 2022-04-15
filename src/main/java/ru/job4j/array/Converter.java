e0023850af88c70a6763c127dd707660041ada7a
ra1kard <91raykard@gmail.com>
2022-04-15 18:37:00 +0300

    3.4. Конвертировать неквадратный двумерный массив в квадратный.

--- src/main/java/ru/job4j/array/Converter.java
+++ src/main/java/ru/job4j/array/Converter.java
@@ -3,28 +3,30 @@ package ru.job4j.array;
 public class Converter {
 
     public static int[][] convertInSquareArray(int[][] array) {
-        int last11 = array[array.length - 1].length;        // длина последней строки
-        int last12 = array[array.length - 1][last11 - 1];   // значение последнего элемента последней строки
-        //System.out.println(last12);     // 11
-
+        int last12 = array[array.length - 1][array[array.length - 1].length - 1];
         int result = 0;
         int n = 0;
         while (result < last12) {
             n++;
             result = n * n;
         }
-        //System.out.println(n);          // 4
-
         int[][] arraySecond = new int[n][n];
+
+        int countX = 0;
+        int countY = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-
-                for (int k = 0; k < j + 1; k++) {
-                    for (int l = 0; l < k + 1; l++) {
-                        arraySecond[k][l] = array[i][j];
-                    }
+                if (countX < n && countY < n) {
+                    arraySecond[countX][countY] = array[i][j];
+                    countY++;
+                } else if (countX < n && countY == n) {
+                    countX++;
+                    countY = 0;
+                    arraySecond[countX][countY] = array[i][j];
+                    countY++;
+                } else if (countX == n) {
+                    break;
                 }
-
             }
         }
 
@@ -36,20 +38,6 @@ public class Converter {
         }
 
         return arraySecond;
-    }
-
-    public static void main(String[] args) {
-        /*int[][] array =     {{1},
-                            {2, 3},
-                            {4, 5, 6},
-                            {7, 8},
-                            {9}};*/
-
-        int[][] array =     {{1},
-                            {2, 3, 4},
-                            {5, 6},
-                            {7, 8, 9, 10, 11}};
-        int[][] rsl = Converter.convertInSquareArray(array);
     }
 
 }
