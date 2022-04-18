ff408ed8f5744f4bb52c2c03a46516810b841bd5
Stanislav Kufarev <91raykard@gmail.com>
2022-04-18 18:08:56 +0300

    3.9. Сумма элементов, пока не выполнится условие.

---
+++ src/main/java/ru/job4j/array/SumWithStopEl.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class SumWithStopEl {
+
+    public static int count(int[] data, int el) {
+        int sum = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                break;
+            } else {
+                sum += data[i];
+            }
+        }
+
+        if (sum % 2 == 0) {
+            return sum;
+        } else {
+            return 0;
+        }
+    }
+
+}
