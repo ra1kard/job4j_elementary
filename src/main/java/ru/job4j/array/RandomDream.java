d457ad022797616174a5bda41c6b2f0b1f23caf0
Stanislav Kufarev <91raykard@gmail.com>
2022-04-19 01:16:40 +0300

    4.8. Поле чудес

---
+++ src/main/java/ru/job4j/array/RandomDream.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class RandomDream {
+
+    public static String random(String[] prizes, int num) {
+        int result;
+        result = num / prizes.length;
+        result = result * prizes.length;
+        result = num - result;
+
+        if (result == 0) {
+            result += 4;
+        }
+
+        return prizes[result - 1];
+    }
+
+}
