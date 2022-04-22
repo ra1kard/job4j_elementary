b9054641f9b25e7d18c25f7da8657d551eb4bfcc
ra1kard <91raykard@gmail.com>
2022-04-22 11:44:06 +0300

    5.2. Объединение множеств

---
+++ src/main/java/ru/job4j/array/OrArray.java
@@ -0,0 +1,79 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class OrArray {
+
+    /**
+     * Собираешь из двух один, потом одинаковые удаляешь, потом сортируешь
+     * */
+
+    public static int[] or(int[] left, int[] right) {
+        int[] resultLeftRight = new int[left.length + right.length];
+        int count = 0;
+        int countWithoutNull = 0;
+
+        for (int i = 0; i < left.length; i++) {
+            resultLeftRight[count] = left[i];
+            count++;
+        }
+        for (int i = 0; i < right.length; i++) {
+            resultLeftRight[count] = right[i];
+            count++;
+        }
+
+        for (int i = 0; i < resultLeftRight.length; i++) {
+            for (int j = 0; j < resultLeftRight.length; j++) {
+                if (j < i && resultLeftRight[i] == resultLeftRight[j]) {
+                    resultLeftRight[j] = 0;
+                } else if (j > i && resultLeftRight[i] == resultLeftRight[j]) {
+                    resultLeftRight[j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < resultLeftRight.length; i++) {
+            if (resultLeftRight[i] != 0) {
+                countWithoutNull++;
+            }
+        }
+
+        for (int i = 0; i < resultLeftRight.length; i++) {
+            for (int j = i + 1; j < resultLeftRight.length; j++) {
+                if (resultLeftRight[i] == 0 && resultLeftRight[j] != 0) {
+                    resultLeftRight[i] = resultLeftRight[j];
+                    resultLeftRight[j] = 0;
+                }
+            }
+        }
+
+        int[] result = Arrays.copyOf(resultLeftRight, countWithoutNull);
+
+        int temp = 0;
+        for (int i = 0; i < result.length; i++) {
+            for (int j = i + 1; j < result.length; j++) {
+                if (result[j] < result[i]) {
+                    temp = result[i];
+                    result[i] = result[j];
+                    result[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        OrArray.or(new int[] {}, new int[] {});
+        OrArray.or(new int[] {1}, new int[] {});
+        OrArray.or(new int[] {}, new int[] {1});
+        OrArray.or(new int[] {1, 2}, new int[] {1, 2});
+        OrArray.or(new int[] {1, 2}, new int[] {1, 2, 3});
+        OrArray.or(new int[] {1}, new int[] {2, 3});
+    }
+
+}
