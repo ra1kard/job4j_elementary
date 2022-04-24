4196ca8723296e880cc248ff0af1f00f4bae4b3b
Stanislav Kufarev <91raykard@gmail.com>
2022-04-24 18:30:04 +0300

    5.3. Разность множеств

--- src/main/java/ru/job4j/array/DiffArray.java
+++ src/main/java/ru/job4j/array/DiffArray.java
@@ -1,38 +1,49 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class DiffArray {
 
     public static int[] diff(int[] left, int[] right) {
         int value = 0;
         int[] result = new int[left.length + right.length];
+        int countWithoutNull = 0;
 
         for (int i = 0; i < left.length; i++) {
             for (int j = 0; j < right.length; j++) {
                 if (left[i] == right[j]) {
-                    value++;                    //увеличиваем, если попадаем на тоже число во втором множестве
+                    value++;
                 }
             }
-            if (value == 0) {                   //запишем если числа нет во втором множестве
+            if (value == 0) {
                 result[i] = left[i];
+            }
+            value = 0;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != 0) {
+                countWithoutNull++;
             }
         }
 
         for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
+            for (int j = i + 1; j < result.length; j++) {
+                if (result[i] == 0 && result[j] != 0) {
+                    result[i] = result[j];
+                    result[j] = 0;
+                }
+            }
         }
 
-        return result;
-    }
+        int[] result2 = Arrays.copyOf(result, countWithoutNull);
 
-    public static void main(String[] args) {
+        for (int i = 0; i < result2.length; i++) {
+            System.out.print(result2[i] + " ");
+        }
+        System.out.println();
 
-        //DiffArray.diff(new int[] {1, 2}, new int[] {2});      //нули лишние
-
-        //DiffArray.diff(new int[] {}, new int[] {});           //пусто     ОК!!!
-        //DiffArray.diff(new int[] {1}, new int[] {});          //1         ОК!!!
-        //DiffArray.diff(new int[] {}, new int[] {1});          //0, а должно быть пусто
-        //DiffArray.diff(new int[] {1, 2, 3}, new int[] {2});   //3 не попала: 1 0 0 0, а должно быть 1 3
-        DiffArray.diff(new int[] {1, 3}, new int[] {2});      //1 3 0, а должно быть 1 3
+        return result2;
     }
 
 }
