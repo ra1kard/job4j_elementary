49fb69f9a3e9e01b40026acdd7df619d715d9356
ra1kard <91raykard@gmail.com>
2022-05-04 21:26:37 +0300

    6.1. Четное или нечетное количество отрицательных значений

---
+++ src/main/java/ru/job4j/array/PositiveOrNegative.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class PositiveOrNegative {
+
+    public static boolean check(int[] data) {
+        int countMinus = 0;
+        int countPlus = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > 0) {
+                countPlus++;
+            } else if (data[i] < 0) {
+                countMinus++;
+            }
+        }
+
+        if (countMinus % 2 != 0) {
+            return true;
+        } else if (countPlus % 2 == 0) {
+            return false;
+        }
+
+        return false;
+    }
+
+}
