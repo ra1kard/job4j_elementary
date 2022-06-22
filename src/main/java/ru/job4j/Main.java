99041ba596378ad554ad71ce7d07b9b5164965bc
Stas Kufarev Notebook <91raykard@gmail.com>
2022-06-22 15:22:27 +0300

    test commit checkstyle

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("test git from new notebook");
+        System.out.println();
     }
 
 }
