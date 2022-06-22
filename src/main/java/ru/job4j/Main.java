225d77517c65657026daf33963e3e14d0803af0e
Stas Kufarev Notebook <91raykard@gmail.com>
2022-06-22 14:40:11 +0300

    test commit

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("test git from new notebook");
     }
 
 }
