a2e5a2949f43c7e3c2a85f63d2fcc0f4e39b4a6c
Stas Kufarev <91raykard@gmail.com>
2022-08-13 21:46:49 +0300

    exercice

--- src/main/java/ru/job4j/array/UpperCase.java
+++ src/main/java/ru/job4j/array/UpperCase.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class UpperCase {
 
     public static char[] toUpperCase(char[] string) {
@@ -17,9 +19,26 @@ public class UpperCase {
         return result;
     }
 
+    public static char[] onlyLowCase(char[] input) {
+        char[] result = new char[input.length];
+        int count = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (Character.isLetter(input[i]) && Character.isLowerCase(input[i])) {
+                result[count] = Character.toUpperCase(input[i]);
+                count++;
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+        }
+        result = Arrays.copyOf(result, count);
+        return result;
+    }
+
     public static void main(String[] args) {
-        char[] input = "alpha-123".toCharArray();
-        UpperCase.toUpperCase(input);
+        char[] input = "aLpHa - 1".toCharArray();
+        //UpperCase.toUpperCase(input);
+        UpperCase.onlyLowCase(input);
     }
 
 }
--- src/test/java/ru/job4j/array/UpperCaseTest.java
+++ src/test/java/ru/job4j/array/UpperCaseTest.java
@@ -13,4 +13,32 @@ public class UpperCaseTest {
         assertArrayEquals(expect, UpperCase.toUpperCase(input));
     }
 
+    @Test
+    public void lettersLowerCaseWithDigits() {
+        char[] input = "alpha012".toCharArray();
+        char[] expect = "ALPHA".toCharArray();
+        assertArrayEquals(expect, UpperCase.onlyLowCase(input));
+    }
+
+    @Test
+    public void lettersLowerCaseWithDigitsWithSymbols() {
+        char[] input = "alpha-12@?-".toCharArray();
+        char[] expect = "ALPHA".toCharArray();
+        assertArrayEquals(expect, UpperCase.onlyLowCase(input));
+    }
+
+    @Test
+    public void lettersLowerCaseAndOneUpperCaseDigitsSymbolsSpace() {
+        char[] input = "aLpha - 1-0!@#$5".toCharArray();
+        char[] expect = "APHA".toCharArray();
+        assertArrayEquals(expect, UpperCase.onlyLowCase(input));
+    }
+
+    @Test
+    public void lettersLowerCaseAndTwoUpperCaseDigitsSpace() {
+        char[] input = "aLpHa - 1".toCharArray();
+        char[] expect = "APA".toCharArray();
+        assertArrayEquals(expect, UpperCase.onlyLowCase(input));
+    }
+
 }
